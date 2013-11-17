b1fad91384b11e40fa2a5ad1f06624abb1ffe5db
Alex Navarrete <banavarretem@unal.edu.co>
2013-11-16 20:38:11 -0500

    First Commit
    This is the first commit. Hoorray!! :D

---
+++ NewExampleProject/src/newexampleproject/NewExampleProject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newexampleproject;
+
+/**
+ *
+ * @author Alex
+ */
+public class NewExampleProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HelloFrame h = new HelloFrame();
+        h.setVisible(true);
+        h.setLocationRelativeTo(null);
+    }
+}
